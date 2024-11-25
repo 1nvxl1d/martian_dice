31d2f41f8af89fa853f775459d97946b11984850
1nvxl1d <27isb10@gmail.com>
2024-11-25 11:46:43 +0300

    11

--- src/Game_Server.py
+++ src/Game_Server.py
@@ -3,10 +3,8 @@ from src.Player import Player
 from src.Hand import Hand
 
 class Game_Server:
-    dice_count = 13
-
     def new_game(self):
-        Game_Server.dice_count = 13
+        Hand.dice_count = 13
         Hand.global_res = []
         Hand.can_chose_human = 1
         Hand.can_chose_cow = 1
--- src/Hand.py
+++ src/Hand.py
@@ -5,39 +5,40 @@ import random
 
 class Hand:
     res = []
-    global_res = []
+    round_res = []
     can_chose_human = 1
     can_chose_cow = 1
     can_chose_chicken = 1
+    dice_count = 13
 
     def roll_dice(self):
-        Hand.res = [random.choice(Dice.SIDES) for _ in range(Game_Server.dice_count)]
+        Hand.res = [random.choice(Dice.SIDES) for _ in range(Hand.dice_count)]
         return Hand.res
 
     def del_tanks(self):
-        Game_Server.dice_count -= Hand.res.count('t')
+        Hand.dice_count -= Hand.res.count('t')
         for _ in range(Hand.res.count('t')):
-            Hand.global_res.append('t')
+            Hand.round_res.append('t')
 
     def chose_beam(self):
-        Game_Server.dice_count -= Hand.res.count('b')
+        Hand.dice_count -= Hand.res.count('b')
         for _ in range(Hand.res.count('b')):
-            Hand.global_res.append('b')
+            Hand.round_res.append('b')
 
     def chose_human(self):
-        Hand.can_del_human = 0
-        Game_Server.dice_count -= Hand.res.count('h')
+        Hand.can_chose_human = 0
+        Hand.dice_count -= Hand.res.count('h')
         for _ in range(Hand.res.count('h')):
-            Hand.global_res.append('h')
+            Hand.round_res.append('h')
 
     def chose_cow(self):
         Hand.can_chose_cow = 0
-        Game_Server.dice_count -= Hand.res.count('c')
+        Hand.dice_count -= Hand.res.count('c')
         for _ in range(Hand.res.count('c')):
-            Hand.global_res.append('c')
+            Hand.round_res.append('c')
 
     def chose_chicken(self):
         Hand.can_chose_chicken = 0
-        Game_Server.dice_count -= Hand.res.count('ch')
+        Hand.dice_count -= Hand.res.count('ch')
         for _ in range(Hand.res.count('ch')):
-            Hand.global_res.append('ch')
+            Hand.round_res.append('ch')
